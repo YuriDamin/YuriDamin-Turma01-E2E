ad491b583a17cda2779c8ba16ee1407e078bb7b0
YuriDamin <yuribonfant@gmail.com>
2024-11-06 20:00:22 -0300

    Commit 2

--- src/scenarios/LoginTest.spec.js
+++ src/scenarios/LoginTest.spec.js
@@ -2,13 +2,13 @@ const { test } = require('@playwright/test');
 const { LoginPage } = require('../pages/LoginPage');
 
 test.describe('OrangeHRM Login Test', () => {
-    test('should login successfully with valid credentials', async ({ page }) => {
-        const loginPage = new LoginPage(page);
+  test('should login successfully with valid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page);
 
-        await loginPage.navigate();
+    await loginPage.navigate();
 
-        await loginPage.login('Admin', 'admin123');
+    await loginPage.login('Admin', 'admin123');
 
-        await loginPage.verifyLoginSuccess();
-    });
+    await loginPage.verifyLoginSuccess();
+  });
 });
